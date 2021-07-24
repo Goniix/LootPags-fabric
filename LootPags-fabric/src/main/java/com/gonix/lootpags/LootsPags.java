08390383a87e163490cb23eceaa65d4edb90d3e4
Goniix <87310572+Goniix@users.noreply.github.com>
2021-07-24 14:14:12 +0200

    Add files via upload

---
+++ LootPags-fabric/src/main/java/com/gonix/lootpags/LootsPags.java
@@ -0,0 +1,11 @@
+package com.gonix.lootpags;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LootsPags implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
